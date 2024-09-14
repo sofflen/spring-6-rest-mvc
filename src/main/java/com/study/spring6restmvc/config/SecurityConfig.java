7fd0b0afc586afe7ae1a3a009d93df474cb70ff4
sofflen <nixxesgaming@gmail.com>
2024-09-14 18:31:49 +0300

    added RestAssured and swagger request validator for it

--- src/main/java/com/study/spring6restmvc/config/SecurityConfig.java
+++ src/main/java/com/study/spring6restmvc/config/SecurityConfig.java
@@ -2,11 +2,13 @@ package com.study.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@Profile("!test")
 public class SecurityConfig {
 
     @Bean
