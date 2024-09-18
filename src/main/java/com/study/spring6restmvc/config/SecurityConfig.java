2aef4e7013756f3a1b468fa2da31f6d7ea463993
sofflen <nixxesgaming@gmail.com>
2024-09-18 11:25:02 +0300

    added Spring Actuator
    updated Security Config
    
    added properties for health probes

--- src/main/java/com/study/spring6restmvc/config/SecurityConfig.java
+++ src/main/java/com/study/spring6restmvc/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.study.spring6restmvc.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    @Order(2)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(registry ->
@@ -21,6 +24,17 @@ public class SecurityConfig {
                                 .anyRequest().authenticated())
                 .oauth2ResourceServer(resourceServer ->
                         resourceServer.jwt(Customizer.withDefaults()));
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(1)
+    SecurityFilterChain actuatorSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeHttpRequests(registry ->
+                        registry.anyRequest().permitAll());
 
         return http.build();
     }
