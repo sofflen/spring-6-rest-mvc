d8608d48be8fb6f2414992dec6f5fdb6b9cbe452
sofflen <nixxesgaming@gmail.com>
2024-09-20 22:02:33 +0300

    added migration script for customer_audit table

--- src/main/java/com/study/spring6restmvc/repositories/CustomerAuditRepository.java
+++ src/main/java/com/study/spring6restmvc/repositories/CustomerAuditRepository.java
@@ -2,8 +2,10 @@ package com.study.spring6restmvc.repositories;
 
 import com.study.spring6restmvc.entities.CustomerAudit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface CustomerAuditRepository extends JpaRepository<CustomerAudit, UUID> {
 }
